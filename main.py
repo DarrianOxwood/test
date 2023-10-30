f93282c99aaa9ba9c8e993e17c17a287d9d58519
Иван Ступко <DarrianCrashOxwood@yandex.ru>
2023-10-30 09:58:53 +0700

    commit #3 counting the number of parts in stock

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ def read_file(filename):
         data = [line.strip().split() for line in lines]
         return data
 
+
+stock_data = read_file('skl.txt')
+
+stock = {}
+
+for item in stock_data:
+    detail, quantity = item
+    if detail in stock:
+        stock[detail] += int(quantity)
+    else:
+        stock[detail] = int(quantity)
+
+print(stock_data)
+print(stock)
