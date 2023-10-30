ff7606ab7fb4f1da9ec8f9759e9ac84ad88a0ff6
Иван Ступко <DarrianCrashOxwood@yandex.ru>
2023-10-30 13:13:56 +0700

    commit #5 creating a plan generation algorithm

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 def read_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         lines = file.readlines()
@@ -17,12 +18,24 @@ for item in stock_data:
         stock[detail] = int(quantity)
 
 
-expenditure_plan_data = read_file('plan.txt')
+plan_data = read_file('plan.txt')
 
-expenditure_plan = []
+purchasing_plan = []
 
-for item in expenditure_plan_data:
+for item in plan_data:
     date, detail, quantity = item
-    expenditure_plan.append(item)
+    stock_quantity = stock.get(detail, 0)
+    deficit = int(quantity) - stock_quantity
+    if deficit > 0:
+        quarter = ((int(date.split('.')[1]) - 1) // 3) + 1
+        year = int(date.split('.')[2])
+        quarter_quantity = int(math.ceil(deficit * 1.2))
+        purchasing_plan.append((
+            4 if quarter - 1 == 0 else quarter - 1,
+            year - 1 if quarter - 1 == 0 else year,
+            detail,
+            quarter_quantity
+        ))
 
-print(expenditure_plan)
+print(stock)
+print(purchasing_plan)
