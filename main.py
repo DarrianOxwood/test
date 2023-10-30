9c31b6e25419a57de63371b5eecb7e7058644bae
Иван Ступко <DarrianCrashOxwood@yandex.ru>
2023-10-30 14:26:15 +0700

    commit #8 creating generate_purchasing_plan_file function

--- main.py
+++ main.py
@@ -8,41 +8,39 @@ def read_file(filename):
         return data
 
 
-stock_data = read_file('skl.txt')
+def generate_purchasing_plan_file(stock_data_input_file, plan_data_input_file, output_plan_file):
+    stock_data = read_file(stock_data_input_file)
+    plan_data = read_file(plan_data_input_file)
 
-stock = {}
+    stock = {}
+    purchasing_plan = []
 
-for item in stock_data:
-    detail, quantity = item
-    if detail in stock:
-        stock[detail] += int(quantity)
-    else:
-        stock[detail] = int(quantity)
+    for item in stock_data:
+        detail, quantity = item
+        if detail in stock:
+            stock[detail] += int(quantity)
+        else:
+            stock[detail] = int(quantity)
 
-plan_data = read_file('plan.txt')
+    for item in plan_data:
+        date, detail, quantity = item
+        stock_quantity = stock.get(detail, 0)
+        deficit = int(quantity) - stock_quantity
+        if deficit > 0:
+            quarter = ((int(date.split('.')[1]) - 1) // 3) + 1
+            year = int(date.split('.')[2])
+            quarter_quantity = int(math.ceil(deficit * 1.2))
+            purchasing_plan.append((
+                4 if quarter - 1 == 0 else quarter - 1,
+                year - 1 if quarter - 1 == 0 else year,
+                detail,
+                quarter_quantity
+            ))
 
-purchasing_plan = []
+    with open(output_plan_file, 'w') as file:
+        for item in purchasing_plan:
+            quarter, year, detail, quarter_quantity = item
+            file.write(f'{quarter}.{year} {detail} {quarter_quantity}\n')
 
-for item in plan_data:
-    date, detail, quantity = item
-    stock_quantity = stock.get(detail, 0)
-    deficit = int(quantity) - stock_quantity
-    if deficit > 0:
-        quarter = ((int(date.split('.')[1]) - 1) // 3) + 1
-        year = int(date.split('.')[2])
-        quarter_quantity = int(math.ceil(deficit * 1.2))
-        purchasing_plan.append((
-            4 if quarter - 1 == 0 else quarter - 1,
-            year - 1 if quarter - 1 == 0 else year,
-            detail,
-            quarter_quantity
-        ))
 
-print(stock)
-print(purchasing_plan)
-
-with open('potreb.txt', 'w') as file:
-    for item in purchasing_plan:
-        quarter, year, detail, quarter_quantity = item
-        file.write(f'{quarter}.{year} {detail} {quarter_quantity}\n')
-
+generate_purchasing_plan_file('skl.txt', 'plan.txt', 'purchase_plan.txt')
