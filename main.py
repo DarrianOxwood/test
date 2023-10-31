d3122d5abcaf9a59480d715a1eeef2956ebd3cc4
Иван Ступко <DarrianCrashOxwood@yandex.ru>
2023-10-31 09:43:25 +0700

    commit #11 creating functions for counting parts: stock_summ plan_summ_for_quarter_year

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import re
 
 quarters = {1: 'I', 2: 'II', 3: 'III', 4: 'IV'}
 
@@ -10,7 +11,66 @@ def read_file(filename):
         return data
 
 
-def generate_purchasing_plan_file(stock_data_input_file, plan_data_input_file, output_plan_file):
+def plan_summ_for_quarter_year(purchase_list):
+    # приводит список вида ['дд.мм.гггг', 'номер_детали', 'количество']
+    # к списку вида ['квартал.год', 'деталь', количество]
+    # и суммирует количество всех деталей
+    # по кварталу году и номреу детали
+
+    purchasing_plan_dictionary = {}
+
+    for item in plan_data:
+        date, detail, quantity = item
+
+        quarter = ((int(date.split('.')[1]) - 1) // 3) + 1
+        year = int(date.split('.')[2])
+
+        purchase_id = f'{4 if quarter - 1 == 0 else quarter - 1}.{year - 1 if quarter - 1 == 0 else year} {detail}'
+
+        if purchase_id in purchasing_plan_dictionary:
+            purchasing_plan_dictionary[purchase_id] += int(quantity)
+        else:
+            purchasing_plan_dictionary[purchase_id] = int(quantity)
+
+    purchasing_plan = []
+
+    for item in purchasing_plan_dictionary:
+        quarter, year, detail = re.split("[. ]+", item)
+        purchasing_plan.append([quarter, year, detail, purchasing_plan_dictionary[item]])
+
+    return purchasing_plan
+
+
+def stock_summ(stock_items):
+    # суммирует количество деталь на складе
+    # и создает словарь вида {'номер_детали': количество}
+
+    stock = {}
+    for item in stock_data:
+        detail, quantity = item
+        if detail in stock:
+            stock[detail] += int(quantity)
+        else:
+            stock[detail] = int(quantity)
+
+    return stock
+
+
+stock_data = read_file('skl.txt')
+plan_data = read_file('plan.txt')
+
+print(stock_summ(stock_data))
+print(plan_summ_for_quarter_year(plan_data))
+
+"""for item in plan_to_quarters(plan_data):
+    purchase_id, quantity = item
+    if purchase_id in purchasing_plan:
+        purchasing_plan[purchase_id] += int(quantity)
+    else:
+        purchasing_plan[purchase_id] = int(quantity)
+"""
+
+'''def generate_purchasing_plan_file(stock_data_input_file, plan_data_input_file, output_plan_file):
     stock_data = read_file(stock_data_input_file)
     plan_data = read_file(plan_data_input_file)
 
@@ -37,13 +97,13 @@ def generate_purchasing_plan_file(stock_data_input_file, plan_data_input_file, o
                 year - 1 if quarter - 1 == 0 else year,
                 detail,
                 quarter_quantity
-            ))
+            ))'''
 
-    with open(output_plan_file, 'w') as file:
+'''    with open(output_plan_file, 'w') as file:
         for item in purchasing_plan:
             quarter, year, detail, quarter_quantity = item
-            file.write(f'{quarters[quarter]}.{year} {detail} {quarter_quantity}\n')
+            file.write(f'{quarters[quarter]}.{year} {detail} {quarter_quantity}\n')'''
 
-
-generate_purchasing_plan_file('skl.txt', 'plan.txt', 'purchase_plan.txt')
+'''generate_purchasing_plan_file('skl.txt', 'plan.txt', 'purchase_plan.txt')
 print("Файл плана закупок успешно обновлен.")
+'''
